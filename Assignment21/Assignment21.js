7fd7ec36126eebe1e3066607a8689e41fcb77e8f
akanksha-mishra-533 <73543393+akanksha-mishra-533@users.noreply.github.com>
2020-12-08 10:27:40 +0530

    Assignment 21 JS codes done

---
+++ Assignment21/Assignment21.js
@@ -0,0 +1,29 @@
+//Addition
+let a=prompt("Give the first number to perform addition");
+let b=prompt("Give the second number to perform addition");
+console.log("addition of the given " +  a  + " + " +  b  + " = " + (parseInt(a)+parseInt(b)));
+
+//Subtraction
+let c=prompt("Give the subtractant");
+let d=prompt("Give the subtrahend");
+console.log("subtraction of the given " +  c  + " - " +  d  + " = " + (parseInt(c)-parseInt(d)));
+
+//Multiplication
+let e=prompt("Give the first number to perform multiplication");
+let f=prompt("Give the second number to perform multiplication");
+console.log("multiplication of the given " +  e  + " * " +  f  + " = " + (parseInt(e)*parseInt(f)));
+
+//Division
+let g=prompt("Give the first number to be divided");
+let h=prompt("Give the second number to be divided by");
+console.log("division of the given " +  g  + " / " +  h  + " = " + (parseInt(g)/parseInt(h)));
+
+//modulo
+let i=prompt("Give the first number to be divided to get the remainder on dividing");
+let j=prompt("Give the second number to be divided by to get the remainder on dividing");
+console.log("remainder of the given " +  i  + " divided by " +  j  + " = " + (parseInt(i)%parseInt(j)));
+
+//Exponentiation
+let k=prompt("Give the base number");
+let l=prompt("Give the exponent of the base");
+console.log("exponentiation of the given " +  k  + " to the power of " +  l  + " = " + (parseInt(k)**parseInt(l)));
